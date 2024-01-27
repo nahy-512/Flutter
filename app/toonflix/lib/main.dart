23406c8af7d0f50e88e065d04c2f51e79a8c15e0
Nahyun Kim <hedghog1004@naver.com>
2024-01-28 02:03:41 +0900

    ✨ Feat: Week3 - 웹툰앱 3.7강 "Icons and Transforms"
    아이콘을 넣고, 컨테이너에 맞게 자를 수 있는 clipBehavior 속성

--- app/toonflix/lib/main.dart
+++ app/toonflix/lib/main.dart
@@ -106,6 +106,7 @@ class App extends StatelessWidget {
                   height: 20,
                 ),
                 Container(
+                  clipBehavior: Clip.hardEdge,
                   decoration: BoxDecoration(
                     color: const Color(0xFF1F2123),
                     borderRadius: BorderRadius.circular(25),
@@ -113,6 +114,7 @@ class App extends StatelessWidget {
                   child: Padding(
                     padding: const EdgeInsets.all(30),
                     child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: [
                         Column(
                           crossAxisAlignment: CrossAxisAlignment.start,
@@ -148,6 +150,17 @@ class App extends StatelessWidget {
                             ),
                           ],
                         ),
+                        Transform.scale(
+                          scale: 2.2,
+                          child: Transform.translate(
+                            offset: const Offset(-5, 12),
+                            child: const Icon(
+                              Icons.euro_rounded,
+                              color: Colors.white,
+                              size: 88,
+                            ),
+                          ),
+                        )
                       ],
                     ),
                   ),
