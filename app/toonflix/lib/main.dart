5329861d4271eb31071a70c974c43a90db07a917
Nahyun Kim <hedghog1004@naver.com>
2024-01-27 19:31:09 +0900

    ✨ Feat: Week3 - 웹툰앱 3.2강 "Button Section"
    버튼 컨테이너 적용

--- app/toonflix/lib/main.dart
+++ app/toonflix/lib/main.dart
@@ -15,6 +15,7 @@ class App extends StatelessWidget {
               horizontal: 40
           ),
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               SizedBox(
                 height: 80,
@@ -41,7 +42,50 @@ class App extends StatelessWidget {
                     ],
                   )
                 ],
-              )
+              ),
+              SizedBox(
+                height: 120,
+              ),
+              Text(
+                'Total Balance',
+                style: TextStyle(
+                    fontSize: 22,
+                    color: Colors.white.withOpacity(0.8)),
+              ),
+              SizedBox(
+                height: 5,
+              ),
+              Text(
+                '\$5 194 482',
+                style: TextStyle(
+                    fontSize: 44,
+                    fontWeight: FontWeight.w600,
+                    color: Colors.white,
+                ),
+              ),
+              SizedBox(height: 30,),
+              Row(
+                children: [
+                  Container(
+                    decoration: BoxDecoration(
+                      color: Color(0xFFF2B33A),
+                      borderRadius: BorderRadius.circular(45)
+                    ),
+                    child: Padding(
+                      padding: EdgeInsets.symmetric(
+                        vertical: 20,
+                        horizontal: 50,
+                      ),
+                      child: Text(
+                        'Transfer',
+                        style: TextStyle(
+                          fontSize: 22,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
             ],
           ),
         )
