54a73f6fabb3b935b3f06bba4101f779b74565d9
Nahyun Kim <hedghog1004@naver.com>
2024-02-04 00:06:11 +0900

    ✨ Feat: Week4 - 웹툰앱 4.1강 "setState"
    State 클래스에게 데이터가 변경되었다고 알리는 setState() 함수를 통해 UI를 업데이트

--- app/Statefull Widget/lib/main.dart
+++ app/Statefull Widget/lib/main.dart
@@ -15,7 +15,9 @@ class _MyAppState extends State<MyApp> {
   int counter = 0;
 
   void onClicked() {
-    counter = counter + 1;
+    setState(() {
+      counter = counter + 1;
+    });
   }
 
   @override
