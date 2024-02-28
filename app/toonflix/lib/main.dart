f9281b18930be6a2c2fd8e4023951d0fe69a37cd
Nahyun Kim <hedghog1004@naver.com>
2024-02-29 02:21:39 +0900

    ✨ Feat: Week6 - 웹툰앱 6.1강 "AppBar"
    #6.1
    HomeScreen 및 앱바 구현

--- app/toonflix/lib/main.dart
+++ app/toonflix/lib/main.dart
@@ -1,14 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:toonflix/screen/home_screen.dart';
 
 void main() {
   runApp(App());
 }
 
 class App extends StatelessWidget {
+  const App({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Container()
+    return const MaterialApp(
+      home: HomeScreen()
     );
   }
 }
