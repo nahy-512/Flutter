54398d9f4e3e9a40f9e9ae89084d281f45c82ff9
Nahyun Kim <hedghog1004@naver.com>
2024-03-03 14:11:53 +0900

    ✨ Feat: Week6 - 웹툰앱 6.10강 "Hero"
    #6.10
    - 웹툰 id를 통해 태그 설정

--- app/toonflix/lib/screen/detail_screen.dart
+++ app/toonflix/lib/screen/detail_screen.dart
@@ -34,24 +34,27 @@ class DetailScreen extends StatelessWidget {
           Row(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Container(
-                width: 250,
-                clipBehavior: Clip.hardEdge,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(15),
-                  boxShadow: [
-                    BoxShadow(
-                      blurRadius: 15,
-                      offset: Offset(10, 10),
-                      color: Colors.black.withOpacity(0.3),
-                    )
-                  ],
-                ),
-                child: Image.network(
-                  thumb,
-                  headers: const {
-                    'Referer': 'https://comic.naver.com',
-                  },
+              Hero(
+                tag: id,
+                child: Container(
+                  width: 250,
+                  clipBehavior: Clip.hardEdge,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(15),
+                    boxShadow: [
+                      BoxShadow(
+                        blurRadius: 15,
+                        offset: Offset(10, 10),
+                        color: Colors.black.withOpacity(0.3),
+                      )
+                    ],
+                  ),
+                  child: Image.network(
+                    thumb,
+                    headers: const {
+                      'Referer': 'https://comic.naver.com',
+                    },
+                  ),
                 ),
               ),
             ],
