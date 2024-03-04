bfeb86fbf337e5355c3b6c69229b86af4059277a
Nahyun Kim <hedghog1004@naver.com>
2024-03-05 04:12:20 +0900

    ♻️ Refactor: toonflix 에피소드 목록 ListView로 변경

--- app/toonflix/lib/screen/detail_screen.dart
+++ app/toonflix/lib/screen/detail_screen.dart
@@ -158,12 +158,7 @@ class _DetailScreenState extends State<DetailScreen> {
                 future: episodes,
                 builder: (context, snapshot) {
                   if (snapshot.hasData) {
-                    return Column(
-                      children: [
-                        for (var episode in snapshot.data!)
-                          Episode(episode: episode, webtoonId: widget.id)
-                      ],
-                    );
+                    return makeEpisodeList(snapshot);
                   }
                   return Container();
                 },
@@ -172,6 +167,22 @@ class _DetailScreenState extends State<DetailScreen> {
           ),
         ),
       ),
+    );
+  }
+
+  ListView makeEpisodeList(AsyncSnapshot<List<WebtoonEpisodeModel>> snapshot) {
+    return ListView.builder(
+      scrollDirection: Axis.vertical,
+      itemCount: snapshot.data!.length,
+      itemBuilder: (context, index) {
+        var episode = snapshot.data![index];
+        return Episode(
+          webtoonId: widget.id,
+          episode: episode,
+        );
+      },
+      shrinkWrap: true,
+      physics: const NeverScrollableScrollPhysics(),
     );
   }
 }
